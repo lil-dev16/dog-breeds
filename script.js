a53facadb0d228bb33399de796180fb75e1afca3
lil-dev16 <olusegunkd2017@gmail.com>
2022-09-05 04:37:35 +0100

    origin master

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let timer;
 const start = async () => {
     const output = await fetch('https://dog.ceo/api/breeds/list/all');
     const data  = await output.json();
@@ -16,13 +17,31 @@ function createBreedList(breed){
     }).join('')}
     </select>`
 }
+let target = 0;
 async function getBreed(breed) {
     if (breed != 'Select a Dog Breed'){
         const rsult = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
         const data = await rsult.json();
 
-        const slide = document.getElementById('img');
-        slide.src= data.message[0];
+        const slide = document.getElementById('slide');
+        if(timer){
+            stopSlide();
+        }
+       
+        changeImage(data);
+        
         console.log(data.message);
     }
+}
+
+function changeImage(gre){
+    timer = setInterval(() => {
+        slide.style.backgroundImage= `url(${gre.message[target]})`;
+        target++;
+    }, 3000);
+}
+
+
+function stopSlide() {
+    clearInterval(timer)
 }
